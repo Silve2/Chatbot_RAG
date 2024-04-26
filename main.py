33fd47ec1e5126a84c313e8414616bc2458ebd30
Silve2 <67950575+Silve2@users.noreply.github.com>
2024-04-26 17:05:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.core.query_engine import CitationQueryEngine
 
 if __name__ == '__main__':
 
@@ -22,9 +23,9 @@ if __name__ == '__main__':
     index = load_index_from_storage(storage_context)
 
     query_engine = CitationQueryEngine.from_args(
-                    index,
-                    similarity_top_k=3,
-                    citation_chunk_size=512,)
+            index,
+            similarity_top_k=2,
+            citation_chunk_size=1024,)
 
     st.title("My favorite chatbot")
 
@@ -43,11 +44,15 @@ if __name__ == '__main__':
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         response = query_engine.query(prompt)
+
+        source_text = ''
         # Display assistant response in chat message container
+
+        for source_node in response.source_nodes:
+            source_text = source_text + '\n' + source_node.get_text()
+
         with st.chat_message("assistant"):
-            st.markdown(response)
+            st.markdown(response,  help=source_text)
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
-
-
 
